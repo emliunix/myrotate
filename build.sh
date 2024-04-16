7fb67725963a7d416548072be26631ec1c33034c
emliunix <emliunix@hotmail.com>
2024-04-16 23:28:02 +0800

    improve build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
+#!/bin/bash
+
+# the args passed to the script
+COMMANDS=(${@-west build -b native_sim_64})
+
 docker run -it --rm \
        --platform=linux/arm64 \
        -v $(pwd)/../mytemp:/work \
        -v $(pwd):/work/myrotate \
        --workdir /work/myrotate \
        -e "ZEPHYR_TOOLCHAIN_VARIANT=host" \
-       myrotate-builder:latest west build -b native_sim_64
+       myrotate-builder:latest ${COMMANDS[@]}
