e0a2044473fe8244d3a358743e0b379af1233b8f
emliunix <emliunix@hotmail.com>
2024-04-18 06:03:14 +0800

    expose rotary sensor as sse data

--- sensor_demo/src/main.rs
+++ sensor_demo/src/main.rs
@@ -1,5 +1,6 @@
 use std::{convert::Infallible, pin::Pin, time::Duration};
 
+use clap::Parser;
 use axum::{response::{sse::{Event, KeepAlive}, Sse}, routing::get, Router};
 use i2c_linux::I2c;
 use serde::Serialize;
@@ -8,7 +9,15 @@ use tokio::net::TcpListener;
 use tokio_stream::{Stream, StreamExt};
 
 const SENSOR_ADDR: u16 = 0x36;
-const ANGLE_REG: u8 = 0x0C;
+const ANGLE_REG: u8 = 0x0E;
+
+#[derive(Debug, Parser)]
+struct AppArgs {
+    #[clap(long, default_value = "/dev/i2c-1")]
+    i2c_bus: String,
+    #[clap(long, default_value = "0.0.0.0:8080")]
+    bind_addr: String,
+}
 
 #[derive(Debug, Error)]
 enum AppError {
@@ -27,18 +36,23 @@ struct RotaryData {
 }
 
 fn rotary_angles() -> Result<impl Stream<Item=Result<Event, Infallible>>, AppError> {
+    tracing::info!("Reading rotary angle data");
     let mut bus = I2c::from_path("/dev/i2c-1")?;
     bus.smbus_set_slave_address(SENSOR_ADDR, false)?;
     let res = tokio_stream::iter(0..)
-        .map(move |_| {
-            let res = bus.smbus_read_word_data(ANGLE_REG)?;
+        .map(move |i| {
+            let mut res = bus.smbus_read_word_data(ANGLE_REG)?;
+            res = res << 8 | res >> 8;
             let angle = (res as f32) / (0x1000 as f32) * 360.0;
-            Result::<Event, AppError>::Ok(Event::default()
-               .event("data")
-               .json_data(RotaryData { angle })?)
+            Result::<Event, AppError>::Ok(
+                Event::default()
+                    .id(i.to_string())
+                    .event("data")
+                    .json_data(RotaryData { angle })?)
 
         })
         .map(|v| {
+            tracing::debug!("Event res: {:?}", v);
             match v {
                 Ok(v) => Ok(v),
                 Err(e) => {
@@ -66,14 +80,15 @@ async fn get_data_rotary() -> Sse<impl Stream<Item=Result<Event, Infallible>> + 
 
 #[tokio::main]
 async fn main() -> Result<(), AppError> {
+    let args = AppArgs::parse();
     tracing_subscriber::fmt()
         .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
         .init();
     let app = Router::new()
-        .route("/data/rotary", get(get_data_rotary));
+        .route("/api/data/rotary", get(get_data_rotary));
     tracing::info!("Starting server");
     axum::serve(
-        TcpListener::bind("0.0.0.0:8080").await?,
+        TcpListener::bind(args.bind_addr).await?,
         app.into_make_service()
     ).await?;
     Ok(())
