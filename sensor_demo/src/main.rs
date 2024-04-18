41cc607549494f287cdddc0d8522f12447b27f9d
emliunix <emliunix@hotmail.com>
2024-04-19 00:25:26 +0800

    reduce duplicate data

--- sensor_demo/src/main.rs
+++ sensor_demo/src/main.rs
@@ -9,6 +9,7 @@ use tokio::net::TcpListener;
 use tokio_stream::{Stream, StreamExt};
 
 const SENSOR_ADDR: u16 = 0x36;
+const CONF_REG: u8 = 0x07;
 const ANGLE_REG: u8 = 0x0E;
 
 #[derive(Debug, Parser)]
@@ -29,7 +30,6 @@ enum AppError {
     AxumError(#[from] axum::Error),
 }
 
-
 #[derive(Debug, Serialize)]
 struct RotaryData {
     angle: f32,
@@ -38,33 +38,40 @@ struct RotaryData {
 fn rotary_angles() -> Result<impl Stream<Item=Result<Event, Infallible>>, AppError> {
     tracing::info!("Reading rotary angle data");
     let mut bus = I2c::from_path("/dev/i2c-1")?;
+    let mut last_res: Option<u16> = None;
     bus.smbus_set_slave_address(SENSOR_ADDR, false)?;
+    // 3 LSBs hysteresis mode
+    bus.smbus_write_byte_data(CONF_REG, 0x0c)?;
     let res = tokio_stream::iter(0..)
-        .map(move |i| {
+        .throttle(Duration::from_millis(10))
+        .map(move |i| -> Result::<Option<(usize, f32)>, AppError> {
             let mut res = bus.smbus_read_word_data(ANGLE_REG)?;
             res = res << 8 | res >> 8;
-            let angle = (res as f32) / (0x1000 as f32) * 360.0;
-            Result::<Event, AppError>::Ok(
-                Event::default()
-                    .id(i.to_string())
-                    .event("data")
-                    .json_data(RotaryData { angle })?)
-
-        })
-        .map(|v| {
-            tracing::debug!("Event res: {:?}", v);
-            match v {
-                Ok(v) => Ok(v),
-                Err(e) => {
-                    tracing::error!("Error reading angle: {}", e);
-                    Ok(Event::default().event("error").data(format!("Error reading angle: {:?}", e)))
+            tracing::trace!("Sensor data: {:?}", res);
+            if let Some(last_angle) = last_res {
+                if res == last_angle {
+                    return Ok(None);
                 }
             }
+            last_res = Some(res);
+            let angle = (res as f32) / (0x1000 as f32) * 360.0;
+            Ok(Some((i, angle)))
         })
-        .throttle(Duration::from_millis(10));
+        .filter_map(|res| {
+            match res {
+                Ok(Some((i, angle))) => Some(Ok(Event::default()
+                              .id(i.to_string())
+                              .event("data")
+                              .json_data(RotaryData { angle }).unwrap())),
+                Err(e) => {
+                    tracing::error!("Error reading angle: {}", e);
+                    Some(Ok(Event::default().event("error").data(format!("Error reading angle: {:?}", e))))
+                }
+                _ => None
+            }
+        });
     Ok(res)
 }
-
 
 async fn get_data_rotary() -> Sse<impl Stream<Item=Result<Event, Infallible>> + Send> {
     let stream: Pin<Box<dyn Stream<Item=Result<Event, Infallible>> + Send>> = match rotary_angles() {
@@ -76,7 +83,6 @@ async fn get_data_rotary() -> Sse<impl Stream<Item=Result<Event, Infallible>> + 
     };
     Sse::new(stream).keep_alive(KeepAlive::default())
 }
-
 
 #[tokio::main]
 async fn main() -> Result<(), AppError> {
